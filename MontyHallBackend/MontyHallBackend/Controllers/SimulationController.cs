0bb3c429a2233cadf0c53f15b48c8d20fc1a8a82
Buddhi Ravihansa <68497399+Buddhi-Ravihansa@users.noreply.github.com>
2023-05-26 03:38:25 +0530

    Web Application Backend

---
+++ MontyHallBackend/MontyHallBackend/Controllers/SimulationController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MontyHallBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimulationController : ControllerBase
+    {
+     
+
+        [HttpPost("calculate")]
+        public IActionResult Simulate(InitialValueDTO values)
+        {
+            {
+                int wins = 0;
+                int lost = 0;
+                Random random = new Random();
+
+                for (int i = 0; i < values.numberOfSimulations; i++)
+                {
+                    int winningDoor = random.Next(1, 4);
+                    int initialChoice = random.Next(1, 4);
+
+                    if (values.changeDoor)
+                    {
+                        if (initialChoice == winningDoor)
+                            lost++;
+                        else
+                            wins++;
+                    }
+                    else
+                    {
+                        if (initialChoice == winningDoor)
+                            wins++;
+                        else
+                            lost++;
+                    }
+                }
+
+                var result = new
+                {
+                    winCount = wins,
+                    total = values.numberOfSimulations
+                };
+
+                return Ok(result);
+            }
+        }
+    }
+}
---
+++ MontyHallBackend/MontyHallBackend/InitialValueDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MontyHallBackend
+{
+    public class InitialValueDTO
+    {
+        public int numberOfSimulations { get; set; }
+       public  bool changeDoor { get; set; }
+    }
+}
